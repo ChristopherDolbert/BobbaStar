834054738eac7d273422528b5aca3da215c8da23
Eudes <36993464+SailorEudes@users.noreply.github.com>
2023-04-16 21:48:47 +0200

    s

--- includes/CMS.php
+++ includes/CMS.php
@@ -7,7 +7,7 @@
 #|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|
 
 	$sitename = "BobbaStar";
-	$url = "https://bobbastar.fr";
+	$url = "http://localhost";
 	$description = "&raquo; Créer ton appart, tchat avec des centaines d'utilisateurs et fais-toi de nouveaux amis.";
 	$imagepath = "".$url."/web-gallery/"; 
 	$keyword = "CMS, GabCMS, Habbo, rétro, jeune, ados, love, kiff, habbo";
