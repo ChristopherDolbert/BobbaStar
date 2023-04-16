17764da60ec1489c48e7306e5f0877a6c74d52d6
Ayrton BARDIOT <ayrton.bardiot@campus-centre.fr>
2023-04-16 21:53:13 +0200

    Connexion fonctionnel

--- includes/SQL.php
+++ includes/SQL.php
@@ -7,10 +7,10 @@
 #|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|
 
 $GabCMS_hote = 'sql.myhabbo.org'; # Le chemin vers le serveur
-    $GabCMS_port = '3306'; # Ne pas toucher
-    $GabCMS_nom_bd = 'bbstrnt'; # Le nom de votre base de données
-    $GabCMS_utilisateur = 'bbstrnt'; # Nom d'utilisateur pour se connecter
-    $GabCMS_mot_passe = 'Ae!71o79e'; # Mot de passe de l'utilisateur pour se connecter
+$GabCMS_port = '3306'; # Ne pas toucher
+$GabCMS_nom_bd = 'bbstrnt'; # Le nom de votre base de données
+$GabCMS_utilisateur = 'bbstrnt'; # Nom d'utilisateur pour se connecter
+$GabCMS_mot_passe = 'Ae!71o79e'; # Mot de passe de l'utilisateur pour se connecter
  
 try {
 $bdd = new PDO('mysql:host='.$GabCMS_hote.';port='.$GabCMS_port.';dbname='.$GabCMS_nom_bd.'', $GabCMS_utilisateur, $GabCMS_mot_passe);
