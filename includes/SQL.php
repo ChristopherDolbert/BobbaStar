eae9384ffb8a779904c562f25fb96bca336959ca
Eudes <36993464+SailorEudes@users.noreply.github.com>
2023-04-16 21:39:24 +0200

    Update SQL.php

--- includes/SQL.php
+++ includes/SQL.php
@@ -6,15 +6,14 @@
 #|																		  #|
 #|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|#|
 
-    $GabCMS_hote = 'sql.myhabbo.org'; # Le chemin vers le serveur
-    $GabCMS_port = '3306'; # Ne pas toucher
-    $GabCMS_nom_bd = 'bbstr'; # Le nom de votre base de données
-    $GabCMS_utilisateur = 'bbstr'; # Nom d'utilisateur pour se connecter
-    $GabCMS_mot_passe = 'Fg55dc8&2'; # Mot de passe de l'utilisateur pour se connecter
+$GabCMS_hote = 'sql.myhabbo.org'; # Le chemin vers le serveur
+$GabCMS_port = '3306'; # Ne pas toucher
+$GabCMS_nom_bd = 'bbstrnt'; # Le nom de votre base de données
+$GabCMS_utilisateur = 'bbstrnt'; # Nom d'utilisateur pour se connecter
+$GabCMS_mot_passe = 'Ae!71o79e'; # Mot de passe de l'utilisateur pour se connecter
  
 try {
 $bdd = new PDO('mysql:host='.$GabCMS_hote.';port='.$GabCMS_port.';dbname='.$GabCMS_nom_bd.'', $GabCMS_utilisateur, $GabCMS_mot_passe);
 } catch (PDOException $e) {
 die('Erreur sur le fichier SQL.php : ' . $e->getMessage());
 }
-?>
