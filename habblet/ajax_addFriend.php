a172b7a9cef5be1b42233e1c3e9c9524034129b8
Christopher Dolbert <36993464+ChristopherDolbert@users.noreply.github.com>
2023-04-27 20:18:46 +0200

    Optimisation #1

--- habblet/ajax_addFriend.php
+++ habblet/ajax_addFriend.php
@@ -9,69 +9,43 @@ include("../config.php");
 $my_id = $user['id'];
 $id = $_POST['accountId'];
 
-$stmt = $bdd->prepare("SELECT * FROM messenger_friendships WHERE user_one_id = :id AND user_two_id = :my_id");
-$stmt->bindParam(':id', $id);
-$stmt->bindParam(':my_id', $my_id);
-$stmt->execute();
-$rows = $stmt->rowCount();
-if ($rows !== 0) {
-	$error = 1;
-	$message = "This person is already your friend.";
-}
+$sql = "SELECT COUNT(*) as count FROM messenger_friendships WHERE (user_one_id = :id AND user_two_id = :my_id) OR (user_one_id = :my_id AND user_two_id = :id)";
+$stmt1 = $bdd->prepare($sql);
+$stmt1->execute([':id' => $id, ':my_id' => $my_id]);
+$rows1 = $stmt1->fetchColumn();
+$message = ($rows1 > 0) ? "Cette personne est déjà votre amie." : null;
+$error = (bool) $message;
 
+$stmt2 = $bdd->prepare("SELECT COUNT(*) FROM messenger_friendships WHERE (user_one_id = :my_id AND user_two_id = :id) OR (user_one_id = :id AND user_two_id = :my_id)");
+$stmt2->execute([':my_id' => $my_id, ':id' => $id]);
+$rows2 = $stmt2->fetchColumn();
+$message = $rows2 ? "Cette personne est déjà votre amie." : '';
+$error = (bool) $rows2;
 
-$stmt = $bdd->prepare("SELECT * FROM messenger_friendships WHERE user_one_id = :my_id AND user_two_id = :id");
-$stmt->bindParam(':my_id', $my_id);
-$stmt->bindParam(':id', $id);
-$stmt->execute();
-$rows = $stmt->rowCount();
-if ($rows !== 0) {
-	$error = 1;
-	$message = "This person is already your friend.";
-}
+$stmt3 = $bdd->prepare("SELECT COUNT(*) FROM messenger_friendrequests WHERE (user_from_id = :my_id AND user_to_id = :id) OR (user_from_id = :id AND user_to_id = :my_id)");
+$stmt3->execute([':my_id' => $my_id, ':id' => $id]);
+$rows3 = $stmt3->fetchColumn();
+$message = $rows3 ? "Vous avez déjà demandé l'amitié de cette personne." : '';
+$error = (bool) $rows3;
 
+$stmt4 = $bdd->prepare("SELECT COUNT(*) FROM messenger_friendrequests WHERE (user_to_id = :my_id AND user_from_id = :id) OR (user_to_id = :id AND user_from_id = :my_id)");
+$stmt4->execute([':my_id' => $my_id, ':id' => $id]);
+$rows4 = $stmt4->fetchColumn();
+$message = $rows4 ? "Cette personne vous a déjà demandé d'être son ami." : '';
+$error = (bool) $rows4;
 
-$stmt = $bdd->prepare("SELECT * FROM messenger_friendrequests WHERE user_from_id = :my_id AND user_to_id = :id");
-$stmt->bindParam(':my_id', $my_id);
-$stmt->bindParam(':id', $id);
-$stmt->execute();
-$rows = $stmt->rowCount();
-if ($rows !== 0) {
-	$error = 1;
-	$message = "You already requested a friendship from this person.";
-}
+$error = $id === $my_id;
+$message = $error ? "Vous ne pouvez pas vous demander en ami." : '';
 
-
-$sql = $bdd->prepare("SELECT * FROM messenger_friendrequests WHERE user_to_id = :my_id AND user_from_id = :id");
-$sql->bindParam(':my_id', $my_id);
-$sql->bindParam(':id', $id);
-$sql->execute();
-$rows = $sql->rowCount();
-if ($rows <> 0) {
-	$error = 1;
-	$message = "This person already requested you to be their friend.";
-}
-
-
-if ($id == $my_id) {
-	$error = 1;
-	$message = "You cannot friend request yourself.";
-}
-
-if ($error <> 1) {
-	$sql = $bdd->query("SELECT MAX(requestid) FROM messenger_friendrequests WHERE user_to_id = '" . $id . "'");
+if (!$error) {
+	$sql = $bdd->prepare("SELECT COALESCE(MAX(requestid), 0) + 1 FROM messenger_friendrequests WHERE user_to_id = :id");
+	$sql->execute([':id' => $id]);
 	$requestid = $sql->fetchColumn();
-	$requestid = $requestid + 1;
-	$insert = $bdd->prepare("INSERT INTO messenger_friendrequests (user_from_id,user_to_id,requestid) VALUES (:my_id,:id,:requestid)");
-	$insert->bindParam(':my_id', $my_id);
-	$insert->bindParam(':id', $id);
-	$insert->bindParam(':requestid', $requestid);
-	if ($insert->execute()) {
-		$message = "Friend request has been sent successfully.";
-	} else {
-		// Handle insert error
-	}
+	$insert = $bdd->prepare("INSERT INTO messenger_friendrequests (user_from_id, user_to_id, requestid) VALUES (:my_id, :id, :requestid)");
+	$insert->execute([':my_id' => $my_id, ':id' => $id, ':requestid' => $requestid]);
+	$message = $insert->rowCount() ? "Friend request has been sent successfully." : '';
 }
+
 
 ?>
 
@@ -79,8 +53,6 @@ if ($error <> 1) {
 	<ul>
 		<li><?php echo $message; ?></li>
 	</ul>
-
-
 	<p>
 		<a href="#" class="new-button done"><b>Done</b><i></i></a>
 	</p>
